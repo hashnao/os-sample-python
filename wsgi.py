6f531899f00c2b4e440c1bc7670dd2928460446c
Naoya Hashimoto <nhashimo@redhat.com>
2016-11-08 21:28:35 +0900

    Change greeting.

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     return "Hello World!"
+    return "Getting Started With Python!"
 
 if __name__ == "__main__":
     application.run()
